48c03be270eb66f18b47bb371cccce18e03b4e67
tomohiro1024 <gongyezhihong732@gmail.com>
2025-05-12 10:34:31 +0900

    アプリケーションIDの変更

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.qr_code_app"
+    namespace = "com.tomohiro.qr_code_app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.qr_code_app"
+        applicationId = "com.tomohiro.qr_code_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/com/example/qr_code_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/qr_code_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.qr_code_app
+package com.tomohiro.qr_code_app
 
 import io.flutter.embedding.android.FlutterActivity
 
